345ea459bbbb8414b371f87ec90ec5364d60e3cc
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-28 14:30:48 -0500

    Write primary about me text

--- src/pages/Text/aboutText.js
+++ src/pages/Text/aboutText.js
@@ -3,14 +3,17 @@ import { Text } from "../../components";
 
 export const aboutMeText = <Text text=
 {
-`Snow Gold Road Plate Fleet Sail ho galleon loot yardarm jury mast marooned scuppers. Lateen sail avast crimp bring a spring upon her cable swab landlubber or just lubber mizzen Gold Road gangway red ensign. Cable main sheet blow the man down barque cutlass clipper fluke Plate Fleet coxswain reef. Quarter Nelsons folly cutlass swab red ensign haul wind hempen halter bucko jack transom.
-Gaff hearties me draft topgallant hands cackle fruit Privateer salmagundi line. Sink me pinnace deadlights ye topmast loot brigantine gaff yawl marooned. Execution dock bounty brigantine scourge of the seven seas carouser log handsomely flogging blow the man down six pounders. Jenny tea cup.`
+`Hi, I'm Nate. I am a Computer Science Major in the Class of 2020 at Wesleyan University, and I use he/him pronouns.
+
+ Since high school I've been answering the question, "What do you want to do?" with "Build Stuff." It's not a very specific goal by any means, but it speaks to my underlying desire to create. That desire has shaped much of my life. Whether it is working on coding, cooking, baking, brewing, or building, I have devoted endless hours to craft.
+
+ I started out at Wesleyan in 2016. I was still trying to figure out what I wanted to study and what my career should look like. Initially a prospective Physics major, I quickly realized that my talents and interests did not fully align with that path. Purely by chance, my pre-major advisor could not see me for a meeting during Add/Drop of my Sophomore Fall, and I met with Prof. Francis Starr, now the IDEAS Director, and learned about the program. It felt like an excellent step towards uniting my Wesleyan education with my desire to create. I dove in headfirst.`
 }
 />;
 
 export const aboutProjectText = <Text text=
 {
-`Long clothes long boat sutler bilge rat lookout coffer loot interloper reef sails landlubber or just lubber. Lanyard pirate fore spanker careen league lugsail dead men tell no tales galleon nipperkin. Chase careen barkadeer brig lad topgallant dance the hempen jig bucko Plate Fleet booty.
+`Fast-forward to right now
 
 Topgallant nipper smartly quarter bring a spring upon her cable cackle fruit brigantine black spot rigging matey. Cutlass aye man-of-war sutler sheet Sink me blow the man down Jack Tar walk the plank reef sails. Line port blow the man down Pieces of Eight lass draft rigging lugger skysail ballast. Bounty crows nest smartly hang the jib scuppers case shot fore Jack Tar belaying pin crack`
 }
