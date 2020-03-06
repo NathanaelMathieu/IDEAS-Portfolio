74b6fafdd81fc448e9b37b4d0759bdd36e813fc1
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-03-06 18:16:28 -0500

    fix index

--- src/resources/projects/template/index.js
+++ src/resources/projects/template/index.js
@@ -2,5 +2,5 @@
 
 export { default as aboutMockup } from "./aboutMockup.jpg";
 export { default as contactMockup } from "./contactMockup.jpg";
-export { default as contact } from "./contact.jpg";
-export { default as projects } from "./projects.jpg";
+export { default as contact } from "./contact.png";
+export { default as projects } from "./projects.png";
