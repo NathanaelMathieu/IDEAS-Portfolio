d53c709f25aaf5ff81408f0728a17e3512f9085d
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-03-06 20:20:18 -0500

    updated pictures

--- src/resources/projects/tag/index.js
+++ src/resources/projects/tag/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable camelcase */
 
 export { default as cageIso } from "./cageIso.jpg";
-export { default as cageWhiteboard } from "./cageWhiteboard.jpg";
 export { default as side } from "./side.jpg";
-export { default as topSketch } from "./topSketch.jpg";
+export { default as cardboard } from "./cardboard.jpg";
--- src/resources/projects/tempist/index.js
+++ src/resources/projects/tempist/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 
-export { default as gantSketch } from "./gantSketch.jpg";
+export { default as ganttSketch } from "./ganttSketch.jpg";
 export { default as loginSketch } from "./loginSketch.jpg";
-export { default as login } from "./login.jpg";
-export { default as gantt } from "./gantt.jpg";
-export { default as task } from "./task.jpg";
+export { default as login } from "./login.png";
+export { default as gantt } from "./gantt.png";
+export { default as task } from "./task.png";
