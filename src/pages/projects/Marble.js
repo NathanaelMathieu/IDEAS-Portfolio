74df4c6d0c3e75489af316279f3f01a622e6a53f
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-03-06 21:42:12 -0500

    Marble run is gtg

--- src/pages/projects/Marble.js
+++ src/pages/projects/Marble.js
@@ -1,11 +1,8 @@
 import "./Marble.scss";
 import { ImageCard, Layout } from "../../components";
 import React, { Component } from "react";
-
-/*
- * Import { }
- *  from "../../resources/projects/marble";
- */
+import { clips, funnel, funnelGif, kids, track }
+ from "../../resources/projects/marble";
 import { t1, t2, t3, t4, t5, t6 } from "./Text/marbleText.js";
 
 export default class Marble extends Component {
@@ -19,9 +16,21 @@ export default class Marble extends Component {
         <div className="Marble">
           <div className="image_text">
             <ImageCard alt="An isometric view of the Cube"
-              size="4x6"
+              size="4x6" image={kids}
               textEnabled text="An isometric view of the Cube"/>
             {t1}
+          </div>
+          <div className="image_text">
+            <ImageCard alt="An isometric view of the Cube"
+              size="4x6" image={funnelGif}
+              textEnabled text="An isometric view of the Cube"/>
+            {t2}
+          </div>
+          <div className="image_text">
+            <ImageCard alt="An isometric view of the Cube"
+              size="4x6" image={clips}
+              textEnabled text="An isometric view of the Cube"/>
+            {t3}
           </div>
         </div>
       </Layout>
